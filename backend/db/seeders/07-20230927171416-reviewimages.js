c990ebee2064d541982fa3c698c713a633dc67fb
h-guertler <guertler99@gmail.com>
2023-09-27 13:18:21 -0400

    added reviewimages seeder

---
+++ backend/db/seeders/07-20230927171416-reviewimages.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { ReviewImage } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await ReviewImage.bulkCreate([
+    {
+      reviewId: 1,
+      url: "revimg.com/1",
+    },
+    {
+      reviewId: 2,
+      url: "revimg.com/2",
+    },
+    {
+      reviewId: 3,
+      url: "revimg.com/3",
+    },
+   ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = "ReviewImage";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1, 2, 3] },
+    }, {});
+  }
+};
