51965d3700cc99a9d52b25741e0154dfbf1cb3d0
h-guertler <guertler99@gmail.com>
2023-09-27 10:02:21 -0400

    added Review model

---
+++ backend/db/migrations/20230927135946-create-review.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      review: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      stars: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
