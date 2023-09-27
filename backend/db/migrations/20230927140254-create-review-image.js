da41bbc29681eae8c0521243e2c1a1a0c0fbcc0e
h-guertler <guertler99@gmail.com>
2023-09-27 10:08:07 -0400

    added ReviewImage model

---
+++ backend/db/migrations/20230927140254-create-review-image.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReviewImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ReviewImages');
+  }
+};
