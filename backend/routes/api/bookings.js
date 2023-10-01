2618d5cb5b4271c5cf6d8c0914ecb2c1fe35fb5b
h-guertler <guertler99@gmail.com>
2023-10-01 16:52:24 -0400

    added get /api/bookings/current

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,11 +1,36 @@
-const { Booking } = require("../../db/models");
+const { Booking, Spot, SpotImage } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+
 const express = require('express');
 
 const router = express.Router();
 
-router.get("/current", async (req, res, next) => {
-    return res.json({ message: "this is get /current" });
-});
+router.get("/current",
+    requireAuth,
+    async (req, res, next) => {
+        const myBookings = await Booking.findAll({ where: { userId: req.user.id },
+            include: [
+                {
+                model: Spot,
+                as: "Spot",
+                attributes: ["id", "ownerId", "address", "city", "state", "country", "lat", "lng", "name", "price", "previewImage"]
+                }
+            ]
+        });
+
+        for (const booking of myBookings) {
+            const spot = await Spot.findByPk(booking.spotId);
+            const spotImage = await SpotImage.findOne({ where: {
+                preview: true,
+                spotId: spot.id,
+            }});
+
+            if (spotImage) {
+                booking.Spot.previewImage = spotImage.url;
+            }
+        }
+        return res.json(myBookings);
+    });
 
 router.put("/:bookingId", async (req, res, next) => {
     return res.json({ message: "this is /:bookingId"} );
