ec2e3abb822bd06fa160857ad56b0f5415669419
h-guertler <guertler99@gmail.com>
2023-11-04 22:31:43 -0400

    reconfigured navigation component

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../store/session";
 
-function ProfileButton() {
+function ProfileButton({user}) {
+    const dispatch = useDispatch();
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+      };
+
+    const ulClassName = "profile-dropdown";
+
+
     return (
-        <div style={{ color: "black", fontSize: "100px" }}>
-            <i className="fa-solid fa-user"></i>
-        </div>
-    )
+        <>
+            <button>
+                <i className="fa-solid fa-user"></i>
+            </button>
+            <ul className="profile-dropdown">
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default ProfileButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,36 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
-
-// is there a user?
-// if no... include links to log in and sign up
-// if yes... include link to log out
-
 
 function Navigation({isLoaded}) {
 
     const currUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch();
 
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
-      };
-
-      let sessionLinks;
+    let sessionLinks;
 
       if (currUser) {
         sessionLinks = (
             <li>
                 <ProfileButton user={currUser}/>
-                <button onClick={logout}>Log Out</button>
             </li>
         );
       } else {
-        sessionLinks (
+        sessionLinks = (
             <li>
                 <NavLink to="/login">Log In</NavLink>
                 <NavLink to="/signup">Sign Up</NavLink>
