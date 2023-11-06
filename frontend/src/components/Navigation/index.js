c8c1f90e4b9b481a36c88ead061cd3993d4b4bd7
h-guertler <guertler99@gmail.com>
2023-11-06 06:59:33 -0500

    added nesting for login menu and logged in user menu

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,44 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import { useSelector } from "react-redux";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 import './Navigation.css';
 
 function Navigation({isLoaded}) {
-
     const currUser = useSelector(state => state.session.user);
-
-    let sessionLinks;
-
-      if (currUser) {
-        sessionLinks = (
-            <li>
-                <ProfileButton user={currUser}/>
-            </li>
-        );
-      } else {
-        sessionLinks = (
-            <li>
-                <OpenModalButton
-                    buttonText="Log In"
-                    modalComponent={<LoginFormModal />}
-                />
-                <OpenModalButton
-                    buttonText="Sign Up"
-                    modalComponent={<SignupFormModal />}
-                />
-            </li>
-        );
-      }
 
     return (
         <ul>
             <li>
                 <NavLink exact to="/">Home</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={currUser}/>
+                </li>
+            )}
         </ul>
     );
 }
