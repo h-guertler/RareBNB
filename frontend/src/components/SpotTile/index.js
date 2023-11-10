1969a2b111a7fce087fd907b024052d82af06b2b
h-guertler <guertler99@gmail.com>
2023-11-10 18:13:41 -0500

    added required info to spot tile

--- frontend/src/components/SpotTile/index.js
+++ frontend/src/components/SpotTile/index.js
@@ -1,26 +1,38 @@
 import React from "react";
-import "./SpotTile.css"
-// Each tile will have:
-    // a square image, which is the spot's previewImage
-    // below the image, spot's "city, state" -- aligned w edge of photo
-    // below city and state, "price" / night -- aligned w edge of photo
-    // below the image, at the same height as city and state: rating as a decimal
-    // to the left of rating: a star icon
-    // <img src={previewImage} alt={`image of ${name}`}/>
+import "./SpotTile.css";
+import testImg from "./hobbitbnb.JPG";
+import { useHistory } from "react-router-dom";
 
 function SpotTile({spot}) {
     const { previewImage, city, state, price, avgRating, name } = spot;
+
+    let ratingString;
+    if (typeof avgRating === "number" && avgRating > 0) {
+        if (avgRating % 1 === 0) {
+            ratingString = `${avgRating}.0`;
+        } else {
+            ratingString = avgRating.toString();
+        }
+    } else {
+        ratingString = "new"
+    }
+
     return (
-        <div className="spot-tile clickable">
-            <img src={previewImage} className="previewImage" alt="spot preview"/>
-            <span className="first-row">
-                <span className="city-and-state">{`${city}, ${state} `}</span>
-                <i class="fas fa-star"></i>
-                <span>{(avgRating >= 1 && avgRating <= 5 ? avgRating : "new")}</span>
-            </span>
-            <span classname="second-row">
-                <span className="price">{` $${price}/night `}</span>
-            </span>
+        <div className="spot-tile clickable tooltip">
+            <span className="tooltiptext">{name}</span>
+            <img src={testImg} className="previewImage" alt="spot preview"/>
+            <div className="tile-text">
+                <div className="first-row">
+                    <div className="city-and-state">{`${city}, ${state} `}</div>
+                    <div className="rating-div">
+                        <i class="fas fa-star"></i>
+                        {ratingString}
+                    </div>
+                </div>
+                <div classname="second-row">
+                    <div className="price">{` $${price}/night `}</div>
+                </div>
+            </div>
         </div>
     )
 }
